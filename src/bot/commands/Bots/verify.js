74ba29435a9a053de30eab727e74f61bc5ee00a5
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-07-24 23:07:10 -0300

    Update verify.js

--- src/bot/commands/Bots/verify.js
+++ src/bot/commands/Bots/verify.js
@@ -15,7 +15,7 @@ module.exports = class extends Command {
     }
 
     async run(message, [user]) {
-        if (!user || !user.bot) return message.channel.send(`Ping a **bot**.`);
+        if (!user || !user.bot) return message.channel.send(`**( <:dsw_mention:1000943619236175892> ) › ${message.author.username}**, Mencione um bot.`);
         let bot = await Bots.findOne({botid: user.id}, { _id: false });
 
         const botUser = await this.client.users.fetch(user.id);
@@ -26,10 +26,10 @@ module.exports = class extends Command {
         
         let owners = [bot.owners.primary].concat(bot.owners.additional)
         let e = new MessageEmbed()
-            .setTitle('Bot Verified')
+            .setTitle('( <:dsw_verified:1000944362492022964> ) | Bot Verificado')
             .addField(`Bot`, `<@${bot.botid}>`, true)
-            .addField(`Owner(s)`, owners.map(x => x ? `<@${x}>` : ""), true)
-            .addField("Mod", message.author, true)
+            .addField(`Desenvolvedor(es)`, owners.map(x => x ? `<@${x}>` : ""), true)
+            .addField(`Analisador`, message.author, true)
             .setThumbnail(botUser.displayAvatarURL({format: "png", size: 256}))
             .setTimestamp()
             .setColor(0x26ff00)
@@ -39,12 +39,12 @@ module.exports = class extends Command {
         owners = await message.guild.members.fetch({user:owners})
         owners.forEach(o => {
             o.roles.add(message.guild.roles.cache.get(role_ids.bot_developer));
-            o.send(`Your bot \`${bot.username}\` has been verified.`)
+            o.send(`( <:dsw_iconBot:1000946700694863923> ) › Seu bot \`${bot.username}\` foi verificado.`)
         })
         message.guild.members.fetch(message.client.users.cache.find(u => u.id === bot.botid)).then(bot => {
             bot.roles.set([role_ids.verified]);
         })
-        message.channel.send(`Verified \`${bot.username}\``);
+        message.channel.send(`( <:dsw_iconBot:1000946700694863923> ) › O bot \`${bot.username}\` foi verificado.`);
     }
 
     async init() {
