2927141a75d0017572f2a70c15e0d1fd17044616
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-07-31 08:06:38 -0300

    Update eval.js

--- src/bot/commands/Utility/eval.js
+++ src/bot/commands/Utility/eval.js
@@ -14,7 +14,7 @@ module.exports = class extends Command {
 
   async run(message, [...params]) {
     try {
-      var evaled = await inspect(eval(message));
+      var evaled = await inspect(eval(message.args?.join(' ')));
     } catch(err) {
       var evaled = err;
     }
