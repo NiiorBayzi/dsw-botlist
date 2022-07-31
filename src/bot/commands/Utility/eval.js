cd56a27230ca57248088da7e85a38b72b748b3bb
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-07-31 07:45:17 -0300

    Update eval.js

--- src/bot/commands/Utility/eval.js
+++ src/bot/commands/Utility/eval.js
@@ -19,6 +19,6 @@ module.exports = class extends Command {
       var evaled = err;
     }
 
-    message.channel.send(`\`\`\`js\n${evaled?.slice(0, 1990)\`\`\``);
+    message.channel.send(`\`\`\`js\n${evaled?.slice(0, 1990)}\`\`\``);
   }
 }
