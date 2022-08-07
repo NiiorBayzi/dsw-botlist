02291997f0d4c59dbd8e23ce4ec35bd4eeb02819
Niior Bayzi <105406377+NiiorBayzi@users.noreply.github.com>
2022-08-07 11:19:22 -0300

    Create eval.js

---
+++ src/bot/commands/Utility/eval.js
@@ -0,0 +1,26 @@
+const { Command } = require('klasa');
+const { inspect } = require('util');
+const { loadImage, createCanvas, Context2d, ...Canvas } = require('canvas');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      runIn: ['text'],
+      aliases: ['ev', 'run'],
+      permissionLevel: 9,
+      description: 'Only developer.',
+      deletable: true
+    });
+  }
+
+  async run(message, [...params]) {
+    try {
+      var evaled = await inspect(eval(message.args?.join(' ')));
+    } catch(err) {
+      var evaled = err;
+    }
+    evaled = `${evaled}`;
+
+    message.channel.send(`\`\`\`js\n${evaled.slice(0, 1990)}\`\`\``);
+  }
+}
